4e10f089ef0e1cc2bdd1428692bff26fcd96fb18
mateojubells <121090104+mateojubells@users.noreply.github.com>
2024-03-06 19:00:01 +0100

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Practica_M07_UF1"
+include(":app")
+ 
