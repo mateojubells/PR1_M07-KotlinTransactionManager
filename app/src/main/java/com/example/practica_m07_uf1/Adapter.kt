6d239d7f4cab9f8df893aba4eb54090c2c07daf1
Joan <joan.radua@gracia.lasalle.cat>
2024-03-19 17:09:09 +0100

    Code Update
    Code update

--- app/src/main/java/com/example/practica_m07_uf1/Adapter.kt
+++ app/src/main/java/com/example/practica_m07_uf1/Adapter.kt
@@ -1,32 +1,52 @@
 package com.example.practica_m07_uf1
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
 
-class Adapter(private val itemList: List<Transaction>) : RecyclerView.Adapter<Adapter.MyViewHolder>() {
+class Adapter(private val itemList: List<Transaction>) : RecyclerView.Adapter<Adapter.ViewHolder>() {
 
-    inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val name: TextView = itemView.findViewById(R.id.nameTextView)
-        val amount: TextView = itemView.findViewById(R.id.amountTextView)
-        val date: TextView = itemView.findViewById(R.id.dateTextView)
-        val type: TextView = itemView.findViewById(R.id.typeTextView)
-    }
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.transactions, parent, false)
-        return MyViewHolder(itemView)
+        return ViewHolder(itemView)
     }
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val transaction = itemList[position]
-        holder.name.text = "Name: ${transaction.name}"
-        holder.amount.text = "Amount: ${transaction.amount}"
-        holder.date.text = "Date: ${transaction.date}"
-        holder.type.text = "Type: ${transaction.type}"
+        holder.bind(transaction,itemList)
     }
+
+
     override fun getItemCount(): Int {
         return itemList.size
     }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(transaction: Transaction, itemList: List<Transaction>){
+            val item: ConstraintLayout = itemView.findViewById(R.id.item)
+            val name: TextView = itemView.findViewById(R.id.nameTextView)
+            val amount: TextView = itemView.findViewById(R.id.amountTextView)
+
+            name.text = "${transaction.name}"
+            amount.text = "Amount: ${transaction.amount}€"
+
+            item.setOnClickListener{
+                val intent = Intent(itemView.context, DetallesActivity::class.java)
+                intent.putExtra("TRANSACTION", transaction)
+                itemView.context.startActivity(intent)
+
+            }
+        }
+
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/practica_m07_uf1/Transaction.kt
+++ app/src/main/java/com/example/practica_m07_uf1/Transaction.kt
@@ -1,9 +1,10 @@
 package com.example.practica_m07_uf1
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 data class Transaction (@SerializedName("name") var name: String,
                        @SerializedName("amount") var amount: Int,
                        @SerializedName("date") var date: String,
-                       @SerializedName("type") var type: String)
+                       @SerializedName("type") var type: String) : Serializable
 
